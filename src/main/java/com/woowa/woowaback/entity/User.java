371b961d233513ff71db0ef3b566116fcf961613
sm-koeunseo <koeunseo@sookmyung.ac.kr>
2024-01-09 23:31:40 -0500

    ADD: rds 연결, gradle 수정

--- src/main/java/com/woowa/woowaback/entity/User.java
+++ src/main/java/com/woowa/woowaback/entity/User.java
@@ -1,24 +1,24 @@
-//package com.woowa.woowaback.entity;
-//
-//import jakarta.persistence.*;
-//import lombok.Builder;
-//import lombok.NoArgsConstructor;
-//import org.springframework.boot.autoconfigure.web.WebProperties;
-//
-//@Builder
-//@Entity
-//@Table // (name="")
-//@NoArgsConstructor
-//public class User {
-//    @Id @GeneratedValue
-//    @Column // (name="")
-//    private Long id;
-//
-//    @Column
-//    private String name;
-//
-//    public User(Long id, String name){
-//        this.id = id;
-//        this.name = name;
-//    }
-//}
+package com.woowa.woowaback.entity;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
+@Builder
+@Entity
+@Table // (name="")
+@NoArgsConstructor
+public class User {
+    @Id @GeneratedValue
+    @Column // (name="")
+    private Long id;
+
+    @Column
+    private String name;
+
+    public User(Long id, String name){
+        this.id = id;
+        this.name = name;
+    }
+}

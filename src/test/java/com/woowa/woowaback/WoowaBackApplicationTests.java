28c360d1122e725ed5b5cbd2a6417f695e0a2da4
yena <yeena114@naver.com>
2024-01-17 11:54:05 +0900

    fix error

--- src/test/java/com/woowa/woowaback/WoowaBackApplicationTests.java
+++ src/test/java/com/woowa/woowaback/WoowaBackApplicationTests.java
@@ -1,13 +1,16 @@
 package com.woowa.woowaback;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class WoowaBackApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads(String[] args) {
+		System.out.println("server on");
+		SpringApplication.run(WoowaBackApplication.class, args);
 	}
 
 }
